c2889b8f7d4488011ff5ad85c7a4969f9217bd2d
MahekK09 <92735859+MahekK09@users.noreply.github.com>
2023-11-06 09:41:15 +0530

    Update constants.dart

--- lib/calling/constants.dart
+++ lib/calling/constants.dart
@@ -1,6 +1,6 @@
 class MyConst {
   // add your appId and appSign from console
-  static const int appId = 848060773;
+  static const int appId =;
   static const String appSign =
-      "36cb6a354939c76356b5a520c20d58c7fc5673dacb1ae0e9fac62c67abba60c5";
+      "";
 }
